614e22418081d0c443d2eba0f93c2bc61850d62a
Vladyslav Bondarenko <bvbasys@gmail.com>
2020-10-19 01:14:44 +0300

    Updates

---
+++ precache-manifest.91fd5109c1fbc23dbebc1fa530619547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1acb96ae441e379b9d5b1ad3c30eeb8",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "33b40bf0b2fb7e8a7c7a",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.a94640af.chunk.css"
+  },
+  {
+    "revision": "d0315fbec85145cc2598",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.bf8a0e9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.bf8a0e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b40bf0b2fb7e8a7c7a",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.ffe86261.chunk.js"
+  },
+  {
+    "revision": "d582fb92c0585c85238e",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9dc11d67.js"
+  }
+]);
